435adc82b8bf6936cd2710253356d0226e70dcc3
PaulBender <paulbender@live.com>
2017-03-08 16:52:03 -0500

    Released plugin

--- inc/plugins/upcomingevents.php
+++ inc/plugins/upcomingevents.php
@@ -37,6 +37,7 @@ function upcomingevents_info()
 		"author"			=> "Starpaul20",
 		"authorsite"		=> "http://galaxiesrealm.com/index.php",
 		"version"			=> "1.0",
+		"codename"			=> "upcomingevents",
 		"compatibility"		=> "18*"
 	);
 }
